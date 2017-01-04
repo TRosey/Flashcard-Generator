073c71944d6890957564a085ca7f82cdc2946c57
TRosey <tomhenryrosenberg@gmail.com>
2017-01-04 14:47:43 -0600

    Add files via upload

---
+++ Flashcard-Generator/basicFlashcard.js
@@ -0,0 +1,10 @@
+module.exports.BasicFlashcard = function(front, back) {
+    this.front = front;
+    this.back = back;
+    this.displayFront = function() {
+        console.log('Front: ' + this.front);
+    };
+    this.displayBack = function() {
+        console.log('Back: ' + this.back);
+    };
+};
---
+++ Flashcard-Generator/clozeFlashcard.js
@@ -0,0 +1,19 @@
+module.exports.ClozeFlashcard = function(text, cloze) {
+    this.text = text;
+    this.cloze = cloze;
+    this.displayFullText = function() {
+        console.log(this.text);
+    };
+    this.displayPartialText = function() {
+        var partialText = this.text.replace(this.cloze, '_____');
+        if (!this.text.includes(this.cloze)) {
+            return 'Error: cannot figure out location of cloze deletion.';
+        } else {
+            return partialText;
+        }
+    };
+    this.partialText = this.displayPartialText();
+    this.displayCloze = function() {
+        console.log(this.cloze);
+    };
+};
